5657c8bc72f80497bb9ff3e358f91a6ad559f368
0xs <0xs@users.noreply.github.com>
2018-03-07 21:32:09 +0200

    Small improvements

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -8,4 +8,10 @@ class HelloTest extends TestCase {
 		$this->assertEquals(Hello::lower('HeLlO'), 'hello');
 	}
 
+
+	public function testWeirdCharacters() {
+		$this->markTestSkipped('Skip weird character test');
+		$this->assertEquals(Hello::lower('ÖÄÅ'), 'öäå');
+	}
+
 }
